f0751ffeef4f738ccd12af6545f7b2f0d68849de
sudeesh <sudeesh.b26@gmail.com>
2021-10-07 08:12:50 +0100

    changes for cache

--- _static/static/js/5.c284c7df.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@sudeeshbaskaran/service-genie"]=this["webpackJsonp@sudeeshbaskaran/service-genie"]||[]).push([[5],{322:function(e,s,a){}}]);
-//# sourceMappingURL=5.c284c7df.chunk.js.map
