cfecf11621e7d92a6692e54b2b44e1ed0c1ff791
Rajesh <wraejesh@gmail.com>
2021-11-07 00:17:31 +0530

    Added Carousel items

--- src/pages/revampedHomepage/carousel/carouselItem.js
+++ src/pages/revampedHomepage/carousel/carouselItem.js
@@ -3,6 +3,8 @@ import StarRatings from 'react-star-ratings';
 import Marker from '../../../images/assets/img/icons/global/marker.svg';
 import './carousel.styles.scss';
 
+import styles from './carousel.styles.scss';
+
 // Create a carousel item component with image and text
 
 function CarouselItemServices({ image, text, onClick }) {
@@ -13,11 +15,13 @@ function CarouselItemServices({ image, text, onClick }) {
         alt="carousel"
         className="service-carousel__item-image"
       ></img>
-      <p className="service-carousel__item-heading"> {text.heading}</p>
-      <p className="service-carousel__item-pricing">{text.pricing}</p>
-      <button className="service-carousel__item-button" onClick={onClick}>
-        Book now
-      </button>
+      <div className="service-carousel__item-content">
+        <p className="service-carousel__item-heading"> {text.heading}</p>
+        <p className="service-carousel__item-pricing">Starts {text.pricing}</p>
+        <button className="service-carousel__item-button" onClick={onClick}>
+          Book now
+        </button>
+      </div>
     </div>
   );
 }
@@ -29,6 +33,7 @@ function CarouselItemMechanic({
   garageOverallRating,
   pricing,
   distance,
+  carouselName = 'mechanic',
 }) {
   return (
     <div className="mechanic-carousel__item">
@@ -47,7 +52,9 @@ function CarouselItemMechanic({
               className="mechanic-carousel__item-address-marker-image"
             ></img>
           </p>
-          <p>{text.address}</p>
+          <p className="mechanic-carousel__item--light-grey-2">
+            {text.address}
+          </p>
         </div>
         <div className="mechanic-carousel__item-reviews mechanic-carousel__item--semibold">
           {garageOverallRating.averageGarageRatings === 'NaN' ? (
@@ -64,7 +71,7 @@ function CarouselItemMechanic({
               />
               <p
                 style={{ margin: '0' }}
-                className="mechanic-carousel__item--semibold"
+                className="mechanic-carousel__item--semibold mechanic-carousel__item--light-grey-1"
               >
                 {garageOverallRating.totalGarageReviews > 1
                   ? `(${garageOverallRating.totalGarageReviews} reviews)`
@@ -75,12 +82,20 @@ function CarouselItemMechanic({
         </div>
         <div className="mechanic-carousel__item-last-child">
           <div className="mechanic-carousel__item-starts-from">
-            <p className="mechanic-carousel__item--semibold">Starts from</p>
-            <p className="mechanic-carousel__item--bold">{text.pricing}</p>
+            <p className="mechanic-carousel__item--semibold mechanic-carousel__item--light-grey-1 block-clear">
+              Starts from
+            </p>
+            <p className="mechanic-carousel__item--bold mechanic-carousel__item--brand-primary-dark block-clear">
+              {text.pricing}
+            </p>
           </div>
-          <div className="mechanic-carousel__item-starts-distance">
-            <p className="mechanic-carousel__item--semibold">Distance</p>
-            <p className="mechanic-carousel__item--bold">{distance}</p>
+          <div className="mechanic-carousel__item-distance">
+            <p className="mechanic-carousel__item--semibold mechanic-carousel__item--light-grey-1 block-clear">
+              Distance
+            </p>
+            <p className="mechanic-carousel__item--bold mechanic-carousel__item--light-grey-2 block-clear">
+              {distance}
+            </p>
           </div>
           <button className="mechanic-carousel__item-button" onClick={onClick}>
             Book now
@@ -91,4 +106,61 @@ function CarouselItemMechanic({
   );
 }
 
-export { CarouselItemServices, CarouselItemMechanic };
+function CarouselItemAccessories({
+  image,
+  text,
+  onClick,
+  garageOverallRating,
+}) {
+  return (
+    <div className="accessories-carousel__item">
+      <img
+        src={image}
+        alt="carousel"
+        className="accessories-carousel__item-image"
+      ></img>
+      <div className="accessories-carousel__item-content">
+        <p className="accessories-carousel__item-heading">{text.heading}</p>
+        <div className="accessories-carousel__item-reviews accessories-carousel__item--semibold">
+          {garageOverallRating.averageGarageRatings === 'NaN' ? (
+            'No Reviews'
+          ) : (
+            <>
+              <StarRatings
+                rating={parseInt(garageOverallRating.averageGarageRatings)}
+                starRatedColor="#eea44d"
+                numberOfStars={5}
+                name="rating"
+                starDimension="20px"
+                starSpacing="3px"
+              />
+              <p
+                style={{ margin: '0' }}
+                className="accessories-carousel__item--semibold accessories-carousel__item--light-grey-1"
+              >
+                {garageOverallRating.totalGarageReviews > 1
+                  ? `(${garageOverallRating.totalGarageReviews} reviews)`
+                  : `(${garageOverallRating.totalGarageReviews} review)`}
+              </p>
+            </>
+          )}
+        </div>
+        <div className="accessories-carousel__item-last-child">
+          <div className="accessories-carousel__item-starts-from">
+            <p className="accessories-carousel__item--bold accessories-carousel__item--brand-primary-dark block-clear">
+              {text.pricing}
+            </p>
+          </div>
+          <button
+            className="accessories-carousel__item-button"
+            onClick={onClick}
+          >
+            Book now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { CarouselItemServices, CarouselItemMechanic, CarouselItemAccessories };
--- src/pages/revampedHomepage/revampedHomePage.jsx
+++ src/pages/revampedHomepage/revampedHomePage.jsx
@@ -5,6 +5,7 @@ import './styles/mixins.scss';
 import {
   CarouselItemMechanic,
   CarouselItemServices,
+  CarouselItemAccessories,
 } from './carousel/carouselItem';
 import Image3 from '../../images/assets/img/servicecentre/centre2.png';
 
@@ -15,19 +16,6 @@ function RevampHomePage() {
       <CarouselItemServices
         image={Image3}
         text={{ heading: 'Title', pricing: '1599' }}
-      />
-      <CarouselItemMechanic
-        image={Image3}
-        text={{
-          heading: 'Adyar Water Wash And Foam Wash',
-          pricing: '1599',
-          address: '38, 3 rd Avenue, 4th Street, Indira Nagar, Adyar',
-        }}
-        garageOverallRating={{
-          averageGarageRatings: 4,
-          totalGarageReviews: 10,
-        }}
-        distance={'5.5'}
       />
     </div>
   );
