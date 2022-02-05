112fc8afddddc351fdcbb5ee191e00df2f571745
sudeesh <sudeesh.b26@gmail.com>
2022-02-05 09:18:00 +0000

    changes for feedback5thfeb

--- src/pages/revampedHomepage/revampedHomePage.jsx
+++ src/pages/revampedHomepage/revampedHomePage.jsx
@@ -31,7 +31,6 @@ const RevampHomePage = ({device}) => {
     // get latest geocode along with latidue and longitude
     useEffect(() => {
       if(servicesNearme){
-        debugger;
       (async () => {
         await fetch(
           `https://api.mapbox.com/geocoding/v5/mapbox.places/${servicesNearme}.json?access_token=${REACT_APP_MAPBOX_ACCESS_TOKEN}`
